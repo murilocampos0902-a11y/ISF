901ea962af2b1fd9a1924f372ea4d67147b03641
joaofiliperosa04-crypto <joaofiliperosa04@gmail.com>
2026-07-02 18:53:23 -0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME || "ItajubaSemFiltro",
-    port: process.env.DB_PORT || 3000,
+    port: process.env.DB_PORT || 3306,
     ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : null
 });
 
